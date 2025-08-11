c40a8191fbba80a4fcb68d0d85ae4dd19908f1d5
tkarombang <tuangkarombang@gmail.com>
2025-08-11 14:39:15 +0800

    FIx - Model/Table and Schema

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,7 @@
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, EmailStr
+from typing import List
 
+# PRODUCT SCHEMA start
 class ProductState(BaseModel):
   nama: str
   price: float
@@ -18,4 +19,41 @@ class Product(ProductState):
 
   class Config:
     orm_mode = True
-    
+# PRODUCT SCHEMA end
+
+
+
+# ORDER ITEM SCHEMA start
+class OrderItemBase(BaseModel):
+  product_id: int
+  quantity: int
+  price: int
+
+class OrderItemCreate(OrderItemBase):
+  pass
+
+class OrderItemResponse(OrderItemBase):
+  id: int
+
+  class Config:
+    from_attributes = True #UNTUK ORM MODE
+# ORDER ITEM SCHEMA end
+
+
+
+# ORDER SCHEMA start
+class OrderBase(BaseModel):
+  customer_name: str
+  customer_email: EmailStr
+  total_price: float
+
+class OrderCreate(OrderBase):
+  item: List[OrderItemCreate]
+
+class OrderResponse(OrderBase):
+  id: int
+  items: List[OrderItemResponse]
+
+  class Config:
+    from_attributes = True
+#ORDER SCHEMA end
